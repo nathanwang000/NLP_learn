a6ee3cc775cdfe8416c4f4830842cc18860cea77
jiaxuan <jiaxuan@umich.edu>
2015-01-27 14:26:31 -0500

    added detailed reason why min_edit_distance works

--- edit_distance.py
+++ edit_distance.py
@@ -6,15 +6,41 @@ def min_edit_distance(X, Y, print_table=True):
     output the levenshtein edit distance between X and Y
 
     Background:
-    levenshtein edit distance is the minimum operation
+    Levenshtein edit distance is the minimum operation
     (insertion (1), deletion (1), subsitution (2))required
     to change one word into the other
     
     Intuition:
-    dynamic programming, taking advantage of the fact that
+    Dynamic programming, taking advantage of the fact that
     the transformation from X to Y can be seen as an operation
     on the last character of the sequence, which can only be
-    insertion, deletion, or subsitution
+    insertion, deletion, or subsitution.
+    Consider a string X of length n and a string Y of length m,
+    partition them into X[-1] and X[:-1], Y[-1] and Y[:-1].
+    The possibilities are
+    1) X[-1] and Y[-1] are not the same
+    possible operations: deletion, subsitution, insertion
+    a) deletion: one operation used
+    so the problem reduced to D[i-1,j] + 1
+    b) subsitution: 2 operations used
+    so the problem reduced to D[i-1,j] + 2
+    c) insertion
+    insert after X[-1] to match Y
+    the question remains: how long a string to insert?
+    the answer is one is sufficient as proved below
+    base case: insert 1 character
+    the problem reduced to D[i,j-1] + 1
+    assert: the total step for k charactor inserted is at most the same
+    as that of 1 character inserted
+    proof:
+    k character inserted uses k steps. If it can be more efficient than 
+    1 character insertion, then it has to reduce the problem to k-1 steps
+    more efficient, it that's true, then 1 step algorithm can as well use
+    k-1 insertion to achieve the same bound.
+    2) X[-1] and Y[-1] are the same
+    a) do nothing
+    so the problem reduced to D[i-1,j-1]
+    b) insertion, deletion, subsitution similar to case 1)
     '''
     N = len(X); M = len(Y)
     D = np.zeros((N+1, M+1))
